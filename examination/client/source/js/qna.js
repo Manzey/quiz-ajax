c8cc05870137856dc1cfb5be48e6655c3d0088ef
David Larsson <manzey@hotmail.se>
2016-11-29 23:21:10 +0100

    Having issues with getting the server data into a variable

--- examination/client/source/js/qna.js
+++ examination/client/source/js/qna.js
@@ -5,8 +5,34 @@
 module.exports = function Qna() {
     var question = document.getElementById("question");
     var answer = document.getElementById("answer");
-    var contentQ = document.createTextNode("Test");
+
+    htmlQna({method: "get", url: "http://vhost3.lnu.se:20080/question/1"}, function(error, response) {
+        if (error) {
+            throw new Error("Network error " + error)
+        }
+
+        console.log(response);
+        //CONTINUE TRYING TO RETURN THE VALUES
+    });
+
+    var contentQ = document.createTextNode(this.test);
     var contentA = document.createTextNode("TestA");
     question.appendChild(contentQ);
     answer.appendChild(contentA);
 };
+
+function htmlQna(config, callback) {
+    var req = new XMLHttpRequest();
+    req.addEventListener("load", function() {
+        if (req.status >= 400) {
+            callback(req.status);
+        }
+
+        callback(null, req.responseText);
+    });
+
+    req.open(config.method, config.url);
+    req.send();
+}
+
+
